b71dd4fe0fe1b1d73ea3ca3b694beed5e63620e0
KevinDu10 <kevinwd2401@gmail.com>
2023-11-17 22:27:20 -0500

    VN implementing

--- Assets/Scripts/Shooter/World2BossStartTrigger.cs
+++ Assets/Scripts/Shooter/World2BossStartTrigger.cs
@@ -1,21 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Diagnostics;
+using System.Threading;
 
 public class World2BossStartTrigger : MonoBehaviour
 {
     public GameObject wall, falseBase;
     public Boss2GMScript GM;
     void OnTriggerEnter2D(Collider2D c){
-        if(c.gameObject.tag=="Player" && Input.GetKey(KeyCode.M)){
+        if(c.gameObject.tag=="Player"){
             wall.SetActive(true);
             falseBase.SetActive(false);
             //start VN
 
-            //Start Bossfight - to be placed somewhere else
-            GM.StartFight();
+            Action onVNFinish = () => 
+            {
+                GM.StartFight();
+                Destroy(gameObject);
+                SaveData.W2VNCompleted = true;
+            };
 
-            Destroy(gameObject);
+            VNMain.Activate(VNMain.MikuCharisStory, onVNFinish, SaveData.W2VNCompleted);
         }
     }
 }
